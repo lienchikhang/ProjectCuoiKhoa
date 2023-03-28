229b63364b71a3761a5b6233bb2d015fe73de0ea
Nguyễn Thành Đạt <21dh113572@st.huflit.edu.vn>
2023-03-28 11:23:45 +0700

    an thanh ActionBar

--- app/src/main/java/com/example/projectcuoikhoa/RegisterActivity.java
+++ app/src/main/java/com/example/projectcuoikhoa/RegisterActivity.java
@@ -32,6 +32,7 @@ public class RegisterActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
         sharedPreferences = getSharedPreferences(Ultils.SHARE_PREFERENCES_APP, Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
         anhXaDuLieu();
